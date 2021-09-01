1a4af2096a299e1c4b74b3a5e50d95c4ff1f2137
Gianluca Cavajani <cavajanig@gmail.com>
2021-09-01 18:27:21 -0300

    exercise 2.2 finished

--- course-information/src/Components/Total.js
+++ course-information/src/Components/Total.js
@@ -1,11 +1,11 @@
-// import React from "react";
+import React from "react";
 
-// const Total = ({ course }) => {
-//   const sum =
-//     course.parts[0].exercises +
-//     course.parts[1].exercises +
-//     course.parts[2].exercises;
-//   return <p>Number of exercises {sum}</p>;
-// };
+const Total = ({ course }) => {
+  const sum =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises;
+  return <p>Number of exercises {sum}</p>;
+};
 
-// export default Total;
+export default Total;
