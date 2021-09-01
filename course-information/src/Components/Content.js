297bb9aa8443fc42b84bec3a0b9f7198ecbc65ea
Gianluca Cavajani <cavajanig@gmail.com>
2021-09-01 18:26:34 -0300

    exercise 2.1 finished

---
+++ course-information/src/Components/Content.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
+  );
+};
+
+export default Content;
