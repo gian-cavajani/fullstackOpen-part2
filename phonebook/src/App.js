ca454e3934b227268fd30d7a718cf2a1afa4ee6b
Gianluca Cavajani <cavajanig@gmail.com>
2021-09-03 09:13:08 -0300

    exercise 2.9 finished

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -4,6 +4,8 @@ const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
+  const [newFilt, setNewFilt] = useState("");
+  const [showFilter, setShow] = useState(true);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -23,16 +25,37 @@ const App = () => {
     setNewNum("");
   };
 
+  const filterName = (event) => {
+    event.preventDefault();
+    setShow(false);
+  };
+
+  const namesToShow = showFilter
+    ? persons
+    : persons.filter((person) => person.name === newFilt);
+
   const handleChangeName = (event) => {
     setNewName(event.target.value);
   };
   const handleChangeNum = (event) => {
     setNewNum(event.target.value);
   };
+  const handleChangeFilt = (event) => {
+    setNewFilt(event.target.value);
+  };
+  const handleShowAll = () => {
+    setShow(true);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={filterName}>
+        filter people:
+        <input type="text" onChange={handleChangeFilt} value={newFilt} />
+        <button type="submit">filter</button>
+      </form>
+      <button onClick={handleShowAll}>show all</button>
       <form onSubmit={addPerson}>
         <div>
           name:{" "}
@@ -45,7 +68,7 @@ const App = () => {
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-      {persons.map((p) => {
+      {namesToShow.map((p) => {
         return (
           <p key={p.id}>
             Name: {p.name}, Number: {p.number}
